cb9389f75cce018b24510d0dfbc3b73fc1e4111e
SunLxy <1011771396@qq.com>
2025-12-22 13:13:04 +0800

    feat: 添加排序

--- packages/shkfdx-ui/src/utils/index.ts
+++ packages/shkfdx-ui/src/utils/index.ts
@@ -30,11 +30,14 @@ export const letterEnum: Record<string, string> = {
 
 export function randomArray<T = any>(array: T[]) {
   const newList = [...array]
-  for (let i = newList.length - 1; i > 0; i--) {
-    // 生成一个0到i之间的随机整数
-    const j = Math.floor(Math.random() * (i + 1));
-    // 交换元素 array[i] 和 array[j]
-    [newList[i], newList[j]] = [newList[j], newList[i]];
+  const sort = localStorage.getItem("sort") !== "false"
+  if (sort) {
+    for (let i = newList.length - 1; i > 0; i--) {
+      // 生成一个0到i之间的随机整数
+      const j = Math.floor(Math.random() * (i + 1));
+      // 交换元素 array[i] 和 array[j]
+      [newList[i], newList[j]] = [newList[j], newList[i]];
+    }
   }
   return newList;
 }
