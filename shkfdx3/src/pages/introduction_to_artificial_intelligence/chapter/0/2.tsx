207f778c1d7a27f7b93558d19b50f3eb64467304
SunLxy <1011771396@qq.com>
2025-12-24 11:11:42 +0800

    fix: 修改

--- shkfdx3/src/pages/introduction_to_artificial_intelligence/chapter/0/2.tsx
+++ shkfdx3/src/pages/introduction_to_artificial_intelligence/chapter/0/2.tsx
@@ -11,17 +11,36 @@ import data8 from "../data/8/2.json"
 import data9 from "../data/9/2.json"
 import data10 from "../data/10/2.json"
 import data11 from "../data/11/2.json"
+import { Button } from "antd"
+import { useMemo } from "react"
 
 const sumList = [...data1, ...data2, ...data3, ...data4, ...data5, ...data6, ...data7, ...data8, ...data9, ...data10, ...data11].map((it, index) => ({ ...it, id: index + 1 }))
 
 const NetworkOne = () => {
-  const { state, dispatch } = useProxyStore({ dataList: randomArray(sumList), isRead: true, isOnlyAnswer: true })
+  const { state, dispatch, proxyInstance } = useProxyStore({ dataList: randomArray(sumList), isRead: true, isOnlyAnswer: true, errorList: [] })
   const dataList = state.dataList as unknown as (QItemProps & { id: string })[]
   const isRead = state.isRead
   const isOnlyAnswer = state.isOnlyAnswer
+  const errorList = state.errorList as unknown as (QItemProps & { id: string })[]
 
   return <MainLayout
-    title="多选题 合集（不含综合试题）"
+    // title="多选题 合集（不含综合试题）"
+    title={
+      <div>
+        多选题 合集（不含综合试题）
+        <Button
+          type="link"
+          onClick={() => {
+            if (errorList.length === 0) {
+              return
+            }
+            dispatch({ dataList: [...errorList].map((it, index) => ({ ...it, id: new Date().getTime() + "_" + index })) as any })
+          }}
+        >
+          提取错误题目
+        </Button>
+      </div>
+    }
   >
     <TipButton
       items={[
@@ -45,17 +64,22 @@ const NetworkOne = () => {
         },
       ]}
     />
-    {dataList.map((item, index) => {
-      return <QItem
-        key={item.id}
-        isRead={isRead}
-        isMulti
-        topic={item.topic}
-        isOnlyAnswer={isOnlyAnswer}
-        options={randomArray(item.options || [])}
-        sort={index + 1}
-      />
-    })}
+    {useMemo(() => {
+      return dataList.map((item, index) => {
+        return <QItem
+          key={item.id}
+          isRead={isRead}
+          isMulti
+          topic={item.topic}
+          isOnlyAnswer={isOnlyAnswer}
+          options={randomArray(item.options || [])}
+          sort={index + 1}
+          onError={() => {
+            dispatch({ errorList: [...proxyInstance.store.errorList, item] as any })
+          }}
+        />
+      })
+    }, [dataList, isRead, isOnlyAnswer])}
   </MainLayout>
 }
 
